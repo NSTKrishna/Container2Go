2db75a2316d8e09f2249fde4d93039c33f44bc8c
NSTKrishna <krishnagehlot936@gmail.com>
2026-05-06 19:33:16 +0530

    update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -182,20 +182,18 @@ func main() {
 		origin := r.Header.Get("Origin")
 		if origin != "" {
 			w.Header().Set("Access-Control-Allow-Origin", origin)
-		}
-		
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-
-		// Reflect whatever headers the frontend is asking to send
-		if headers := r.Header.Get("Access-Control-Request-Headers"); headers != "" {
-			w.Header().Set("Access-Control-Allow-Headers", headers)
-		} else {
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Vary", "Origin")
 		}
 
 		if r.Method == http.MethodOptions {
-			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			if headers := r.Header.Get("Access-Control-Request-Headers"); headers != "" {
+				w.Header().Set("Access-Control-Allow-Headers", headers)
+			} else {
+				w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			}
+			w.WriteHeader(http.StatusNoContent)
 			return
 		}
 		mux.ServeHTTP(w, r)
