ebd549f9387231f064eec0f613426926cb56eae6
NSTKrishna <krishnagehlot936@gmail.com>
2026-05-06 19:19:09 +0530

    update

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -181,12 +181,18 @@ func main() {
 	corsMux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		origin := r.Header.Get("Origin")
 		if origin != "" {
-			// Reflect the origin back to allow credentials from any dev host
 			w.Header().Set("Access-Control-Allow-Origin", origin)
 		}
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		// Reflect whatever headers the frontend is asking to send
+		if headers := r.Header.Get("Access-Control-Request-Headers"); headers != "" {
+			w.Header().Set("Access-Control-Allow-Headers", headers)
+		} else {
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		}
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusOK)
